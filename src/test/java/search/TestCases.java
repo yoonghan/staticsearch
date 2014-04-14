dc1c8457caaf0ae1b622f2390bba49dac13c8389
han <mailyoonghan@gmail.com>
2014-04-14 11:12:44 +0100

    Add Support for java 1.8. Collection has been modified.

--- src/test/java/search/TestCases.java
+++ src/test/java/search/TestCases.java
@@ -2,7 +2,12 @@ package search;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -167,6 +172,94 @@ class StringOrderComparison implements java.util.Comparator<String>{
 	public int compare(String o1, String o2) {
 		return o1.compareTo(o2);
 	}
+
+	public static <T, U extends Comparable<? super U>> Comparator<T> comparing(
+			Function<? super T, ? extends U> arg0) {
+		return null;
+	}
+
+	public static <T, U> Comparator<T> comparing(
+			Function<? super T, ? extends U> arg0, Comparator<? super U> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T> Comparator<T> comparingDouble(
+			ToDoubleFunction<? super T> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T> Comparator<T> comparingInt(ToIntFunction<? super T> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T> Comparator<T> comparingLong(ToLongFunction<? super T> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T extends Comparable<? super T>> Comparator<T> naturalOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T> Comparator<T> nullsFirst(Comparator<? super T> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T> Comparator<T> nullsLast(Comparator<? super T> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T extends Comparable<? super T>> Comparator<T> reverseOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Comparator<String> reversed() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Comparator<String> thenComparing(Comparator<? super String> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public <U extends Comparable<? super U>> Comparator<String> thenComparing(
+			Function<? super String, ? extends U> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public <U> Comparator<String> thenComparing(
+			Function<? super String, ? extends U> arg0,
+			Comparator<? super U> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Comparator<String> thenComparingDouble(
+			ToDoubleFunction<? super String> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Comparator<String> thenComparingInt(
+			ToIntFunction<? super String> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Comparator<String> thenComparingLong(
+			ToLongFunction<? super String> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
 
@@ -174,6 +267,96 @@ class SimpleDataOrderComparison implements java.util.Comparator<SimpleDataType>{
 
 	public int compare(SimpleDataType o1, SimpleDataType o2) {
 		return o1.getValue1().compareTo(o2.getValue1());
+	}
+
+	public static <T, U extends Comparable<? super U>> Comparator<T> comparing(
+			Function<? super T, ? extends U> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T, U> Comparator<T> comparing(
+			Function<? super T, ? extends U> arg0, Comparator<? super U> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T> Comparator<T> comparingDouble(
+			ToDoubleFunction<? super T> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T> Comparator<T> comparingInt(ToIntFunction<? super T> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T> Comparator<T> comparingLong(ToLongFunction<? super T> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T extends Comparable<? super T>> Comparator<T> naturalOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T> Comparator<T> nullsFirst(Comparator<? super T> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T> Comparator<T> nullsLast(Comparator<? super T> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static <T extends Comparable<? super T>> Comparator<T> reverseOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Comparator<SimpleDataType> reversed() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Comparator<SimpleDataType> thenComparing(
+			Comparator<? super SimpleDataType> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public <U extends Comparable<? super U>> Comparator<SimpleDataType> thenComparing(
+			Function<? super SimpleDataType, ? extends U> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public <U> Comparator<SimpleDataType> thenComparing(
+			Function<? super SimpleDataType, ? extends U> arg0,
+			Comparator<? super U> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Comparator<SimpleDataType> thenComparingDouble(
+			ToDoubleFunction<? super SimpleDataType> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Comparator<SimpleDataType> thenComparingInt(
+			ToIntFunction<? super SimpleDataType> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Comparator<SimpleDataType> thenComparingLong(
+			ToLongFunction<? super SimpleDataType> arg0) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
